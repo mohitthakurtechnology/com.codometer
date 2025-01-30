6f8d0fd45001888ef1ffa714c1566b8e83f6ee66
Nothing <mohit.thakur.technology@gmail.com>
2025-01-30 16:13:27 +0530

    canSum DP Problem

--- com.codometer/src/main/java/com/codometer/dp/CanSum_Memoization.java
+++ com.codometer/src/main/java/com/codometer/dp/CanSum_Memoization.java
@@ -1,13 +1,33 @@
 package com.codometer.dp;
 
 /**
- * Hello world!
- *
+ * canSum problem is to return true/false if the target sum is achieved with any combination of array
+ * elements with memoization approach
  */
+
 public class CanSum_Memoization 
 {
+	public static boolean canSum(int targetSum,int []numbersArray)
+	{
+		int remainder;
+		
+		if(targetSum == 0)
+				return true;
+		if(targetSum < 0)
+			return false;
+		
+		for(int number : numbersArray)
+		{
+			remainder = targetSum - number;
+			if(canSum(remainder,numbersArray) == true)
+				return true;
+		} 
+		return false;
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int []numberArray = {10,20,30,23,4,54};
+    	System.out.println(CanSum_Memoization.canSum(50, numberArray));
     }
 }
---
+++ com.codometer/src/test/java/com/codometer/DynamicProgramming.md
@@ -0,0 +1,13 @@
+# Dynamic Programming 
+	There are 2 keys to the DP which would solve the algorithmic and coding 
+	as well as the real life problems in the day to day activity we are involved in
+	First key is the Recursive Approach with Memoization to optimize the solution
+	where we use the tree data structure to analyze the time and space complexity 
+	with the big o 	notation.(number of inputs * amount of work done per input)
+	Memoization is kind of caching your recursive call to reduce the cost or to avoid the costly operation.
+		- Decision making
+		- combination -> like permutation , combination
+		- Optimize out of combinations
+
+	Second key is the Tabulation where we use the table or array to sort the problem
+	look for seeds in the problem before you design or build the sowing process for a tree.
